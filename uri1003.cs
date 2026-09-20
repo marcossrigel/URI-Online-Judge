e4902b8074153559f6aba8ebcf295f0b8e7bf034
Marcos Rigel <marcosrigel1999@gmail.com>
2026-09-20 02:54:40 -0300

    simple sum

---
+++ uri1003.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int soma = a + b;
+
+            Console.WriteLine("SOMA = " + soma);
+
+        }
+    }
+}
