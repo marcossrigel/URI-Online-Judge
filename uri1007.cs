fd560ff5d544bc58f940ccb36eba15e9e6b5f349
Marcos Rigel <marcosrigel1999@gmail.com>
2026-09-20 02:57:33 -0300

    difference

---
+++ uri1007.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+
+            int diferenca = (a * b) - (c * d);
+
+            Console.WriteLine("DIFERENCA = " + diferenca);
+
+        }
+    }
+}
