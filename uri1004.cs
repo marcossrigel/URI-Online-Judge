322afdc1c180df8e824f91d3497259ffb7ed88ba
Marcos Rigel <marcosrigel1999@gmail.com>
2026-09-20 02:55:11 -0300

    simple prod

---
+++ uri1004.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Globalization;
+
+namespace PrimeiroProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int prod = a * b;
+
+            Console.WriteLine("PROD = " + prod);
+
+        }
+    }
+}
